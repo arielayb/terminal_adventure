e39df1c37406312d77d52eae5fd7ce2bedd0493b
arielayb <cyberspace009@bitbucket.org>
2024-01-06 14:06:15 -0700

    added window full screen and res

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
-use std::io;
+use bevy::prelude::*;
+use bevy::window::*;
+use bevy::diagnostic::LogDiagnosticsPlugin;
+use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+use bevy_ascii_terminal::{prelude::*, TiledCameraBundle};
 
-fn main() {
-    println!(r"
-    _____                   _             _  
-   |_   _|__ _ __ _ __ ___ (_)_ __   __ _| | 
-     | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | | 
-     | |  __/ |  | | | | | | | | | | (_| | | 
-     |_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_| 
-     / _ \__   _____ _ __| | ___   __ _  __| |
-    | | | \ \ / / _ \ '__| |/ _ \ / _` |/ _` |
-    | |_| |\ V /  __/ |  | | (_) | (_| | (_| |
-     \___/  \_/ \___|_|  |_|\___/ \__,_|\__,_|");
+#[derive(Component)]
+pub struct GameTerminal;
+
+pub const VIEWPORT_SIZE: [u32;2] = [80,40];
+pub const UI_SIZE: [u32;2] = [VIEWPORT_SIZE[0],8];
+pub const GAME_SIZE: [u32;2] = [VIEWPORT_SIZE[0], VIEWPORT_SIZE[1] - UI_SIZE[1]];
+
+fn setup(mut commands: Commands) {
+    // Create the terminal
+    let term_y = VIEWPORT_SIZE[1] as u32 / 2 - GAME_SIZE[1] as u32 / 2; 
+    let term = Terminal::new([20, term_y]).with_border(Border::single_line());
  
-    loop {
-    }
+    let term_bundle = TerminalBundle::from(term).with_size([GAME_SIZE[0], GAME_SIZE[1] + 2]);
+
+    commands.spawn(term_bundle).insert(GameTerminal);
+
+    let mut terminal = Terminal::new([20,3]).with_border(Border::single_line());
+    // Draw a blue "Hello world!" to the terminal
+    terminal.put_string([1, 1], "Terminal Overload".fg(Color::WHITE));
+
+    commands.spawn((
+        // Spawn the terminal bundle from our terminal
+        TerminalBundle::from(terminal),
+    ));
+
+    let totalx = GAME_SIZE[0];
+    let totaly = GAME_SIZE[1] + UI_SIZE[1];
+
+    commands.spawn((TiledCameraBundle::new().with_tile_count([totalx, totaly]), AutoCamera,));
+    
+}
+
+fn main () {
+    App::new()
+    .add_plugins((DefaultPlugins.set(WindowPlugin{
+        primary_window: Some(Window {
+            fit_canvas_to_parent: true, 
+            present_mode: PresentMode::AutoVsync, 
+            mode: WindowMode::Fullscreen, 
+            resolution: (1920., 1080.).into(), 
+            title: "Terminal Overload".into(), 
+            window_theme: Some(WindowTheme::Dark), 
+            visible: true,
+            ..Default::default()}),
+            ..Default::default()
+    }),  
+    LogDiagnosticsPlugin::default(),
+    FrameTimeDiagnosticsPlugin,
+    TerminalPlugin))
+    .add_systems(Startup, setup)
+    .insert_resource(ClearColor(Color::BLACK))
+    .run();
 }
