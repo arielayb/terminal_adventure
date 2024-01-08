35277bc8d33f6eb466c046e3bd7de868c5329634
arielayb <cyberspace009@gmail.com>
2024-01-07 22:32:34 -0700

    trying to add image

--- src/menu_cursor.rs
+++ src/menu_cursor.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_ascii_terminal::{prelude::*, TiledCameraBundle};
 
 use crate::selection::Position;
 
@@ -8,6 +9,7 @@ const STARTING_CURSOR_TRANSLATION: Vec3 = Vec3::new(30., 8., 0.);
 struct MenuCursorBundle{
     position: Position,
     cursor: Handle<Image>,
+    sceneBundle: SceneBundle,
 }
 
 pub struct MenuCursorPlugin;
@@ -24,6 +26,10 @@ fn spawn_cursor(mut commands: Commands, asset_server: Res<AssetServer>){
             x: 30.,
             y: 8., 
         },
-        cursor: cursor_model,
+        cursor: cursor_model.into(),
+        sceneBundle: SceneBundle{
+            scene: asset_server.load("right.png"),
+            ..default()
+        }
     });
 }
