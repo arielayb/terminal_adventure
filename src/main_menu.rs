15c6a1a032a6d3efa28ac6f0085674e492ed7bd4
arielayb <cyberspace009@gmail.com>
2024-01-28 15:27:23 -0700

    removing dir

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -1,5 +1,4 @@
-use bevy::
-{
+use bevy::{
     core_pipeline::{
         bloom::{BloomCompositeMode, BloomSettings},
         tonemapping::Tonemapping,
@@ -32,7 +31,7 @@ struct Options {
     bot_sel: bool,
 }
 
-fn title_scene(mut commands: Commands, asset_server: Res<AssetServer>) {    
+fn title_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/alphacorsa.personal-use.ttf");
     let text_style = TextStyle {
         font: font.clone(),
@@ -51,49 +50,73 @@ fn title_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         BloomSettings::default(), // 3. Enable bloom for the camera
     ));
-    
-    commands.spawn((SpriteBundle {
-        texture: asset_server.load("right.png"),
-        transform: Transform::from_xyz(-280., -150., 0.),
-        ..default()
-    },
-    Selection::Top, Options{top_sel: false, mid_sel: false, bot_sel: false}));
 
-    commands.spawn(
-Text2dBundle {
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("right.png"),
+            transform: Transform::from_xyz(-280., -150., 0.),
+            ..default()
+        },
+        Selection::Top,
+        Options {
+            top_sel: false,
+            mid_sel: false,
+            bot_sel: false,
+        },
+    ));
+
+    commands.spawn(Text2dBundle {
         text: Text::from_section("|- TERMINAL OVERLORD -|", text_style.clone()),
         transform: Transform::from_xyz(5., 100., 0.),
         ..default()
     });
 
-    commands.spawn(
-Text2dBundle {
+    commands.spawn(Text2dBundle {
         text: Text::from_section("New Game", text_style.clone()),
         transform: Transform::from_xyz(-20., -150., 0.),
         ..default()
     });
 
-    commands.spawn(
-Text2dBundle {
+    commands.spawn(Text2dBundle {
         text: Text::from_section("Load Save", text_style.clone()),
         transform: Transform::from_xyz(-20., -250., 0.),
         ..default()
     });
 
-    commands.spawn(
-Text2dBundle {
+    commands.spawn(Text2dBundle {
         text: Text::from_section("Exit", text_style.clone()),
         transform: Transform::from_xyz(-148., -350., 0.),
         ..default()
     });
 }
 
-fn selector(keys: Res<Input<KeyCode>>, mut sprite_position: Query<(&mut Options, &mut Selection, &mut Transform)>) {
+fn selector(
+    keys: Res<Input<KeyCode>>,
+    mut sprite_position: Query<(&mut Options, &mut Selection, &mut Transform)>,
+) {
     for (mut opt, mut logo, mut transform) in &mut sprite_position {
-        match *logo{
-            Selection::Top => transform.translation = Vec3{x:-280., y:-150., z:0.},
-            Selection::Middle => transform.translation = Vec3{x:-280., y:-250., z:0.},
-            Selection::Bottom => transform.translation = Vec3{x:-280., y:-350., z:0.},    
+        match *logo {
+            Selection::Top => {
+                transform.translation = Vec3 {
+                    x: -280.,
+                    y: -150.,
+                    z: 0.,
+                }
+            }
+            Selection::Middle => {
+                transform.translation = Vec3 {
+                    x: -280.,
+                    y: -250.,
+                    z: 0.,
+                }
+            }
+            Selection::Bottom => {
+                transform.translation = Vec3 {
+                    x: -280.,
+                    y: -350.,
+                    z: 0.,
+                }
+            }
         }
 
         if (keys.just_pressed(KeyCode::W) || keys.just_pressed(KeyCode::Up)) && !opt.top_sel {
@@ -101,22 +124,22 @@ fn selector(keys: Res<Input<KeyCode>>, mut sprite_position: Query<(&mut Options,
             opt.top_sel = true;
             opt.mid_sel = false;
             opt.bot_sel = false;
-
-        }else if (keys.just_pressed(KeyCode::S) || keys.just_pressed(KeyCode::Down)) && !opt.mid_sel { 
+        } else if (keys.just_pressed(KeyCode::S) || keys.just_pressed(KeyCode::Down))
+            && !opt.mid_sel
+        {
             *logo = Selection::Middle;
             opt.mid_sel = true;
             opt.top_sel = false;
             opt.bot_sel = false;
-        
-        }else if (keys.just_pressed(KeyCode::S) || keys.just_pressed(KeyCode::Down)) && !opt.bot_sel {
+        } else if (keys.just_pressed(KeyCode::S) || keys.just_pressed(KeyCode::Down))
+            && !opt.bot_sel
+        {
             *logo = Selection::Bottom;
             opt.bot_sel = true;
             opt.mid_sel = true;
             opt.top_sel = false;
         }
-   
-   
+
+        //assert_nq!(*logo, null);
     }
-   
-            
 }
