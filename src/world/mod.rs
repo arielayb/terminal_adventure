d71fb92c5ea3253d5dd401d44397a6ff7a737049
arielayb <cyberspace009@gmail.com>
2024-02-11 16:58:53 -0700

    working on player controller

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -39,50 +39,54 @@ impl Plugin for World {
 
 fn player_movement(
     keys: Res<Input<KeyCode>>,
-    mut aseprites: ParamSet<(
-        Query<&mut AsepriteAnimation, With<PlayerTag>>,
-    )>,
-){
-    if keys.just_pressed(KeyCode::W) {
-        for mut player_anim in aseprites.p0().iter_mut() {
+    mut aseprites: ParamSet<(Query<&mut AsepriteAnimation, With<PlayerTag>>,)>,
+) {
+    for mut player_anim in aseprites.p0().iter_mut() {
+        if keys.pressed(KeyCode::W) {
+            //for mut player_anim in aseprites.p0().iter_mut() {
             *player_anim = AsepriteAnimation::from(sprites::Player::tags::PLAYER_UP);
+            //}
         }
-    }
 
-    if keys.just_pressed(KeyCode::A) {
-        for mut player_anim in aseprites.p0().iter_mut() {
+        if keys.pressed(KeyCode::A) {
+            //for mut player_anim in aseprites.p0().iter_mut() {
             *player_anim = AsepriteAnimation::from(sprites::Player::tags::PLAYER_LEFT);
+            //}
         }
-    } 
 
-    if keys.just_pressed(KeyCode::D) {
-        for mut player_anim in aseprites.p0().iter_mut() {
+        if keys.pressed(KeyCode::D) {
+            //for mut player_anim in aseprites.p0().iter_mut() {
             *player_anim = AsepriteAnimation::from(sprites::Player::tags::PLAYER_RIGHT);
+            //}
         }
-    }
 
-    if keys.just_pressed(KeyCode::S) {
-        for mut player_anim in aseprites.p0().iter_mut() {
+        if keys.just_pressed(KeyCode::S) && player_anim.is_paused() {
+            //for mut player_anim in aseprites.p0().iter_mut() {
             *player_anim = AsepriteAnimation::from(sprites::Player::tags::PLAYER_DOWN);
-        }
-    } else {
-        for mut player_anim in aseprites.p0().iter_mut() {
+        } else if keys.pressed(KeyCode::S) {
+            //for mut player_anim in aseprites.p0().iter_mut() {
             *player_anim = AsepriteAnimation::from(sprites::Player::tags::PLAYER_DOWN_IDLE);
+            player_anim.toggle();
+
+            //}
+            //}
+            //}
         }
     }
-
 }
 
-fn world_setup(mut commands: Commands,asset_server: Res<AssetServer>) {
+fn world_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(AsepriteBundle {
-        aseprite: asset_server.load("workers/workers1.aseprite"),
-        animation: AsepriteAnimation::from("player_left"),
-        transform: Transform {
-            scale: Vec3::splat(1.),
-            translation: Vec3::new(0., 80., 0.),
+    commands
+        .spawn(AsepriteBundle {
+            aseprite: asset_server.load("workers/workers1.aseprite"),
+            animation: AsepriteAnimation::from("player_left"),
+            transform: Transform {
+                scale: Vec3::splat(1.),
+                translation: Vec3::new(0., 80., 0.),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        ..Default::default()
-    }).insert(PlayerTag);
+        })
+        .insert(PlayerTag);
 }
